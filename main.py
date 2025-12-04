be9f7ce2be5a6f04eacd452c9381c09af88f2ebe
octaviopenaa <octaviopenaa@gmail.com>
2025-12-04 03:06:32 -0300

    primer commit

---
+++ main.py
@@ -0,0 +1,14 @@
+class Producto:
+    def __init__(self, id, nombre, precio, stock):
+        self.id = id
+        self.nombre = nombre
+        self.precio = precio
+        self.stock = stock
+    
+    def a_diccionario(self):
+        diccionario = {
+            "id" = self.id
+            "nombre" = self.nombre
+            "precio" = self
+
+        }
